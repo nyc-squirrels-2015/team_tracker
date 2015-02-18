b7a5de6eda5f16332737a4d750bad48580d7dc41
Kenn <kjb085@gmail.com>
2015-02-18 12:32:39 -0500

    Included a route to allow upvoting to work without AJAX (for the time being)

--- app/controllers/team.rb
+++ app/controllers/team.rb
@@ -1,3 +1,7 @@
+get '/' do
+  @teams = Team.all.order(score: :desc)
+  erb :main
+end
 
 get '/teams/new' do
   @players = Player.all.order(last_name: :asc)
@@ -16,6 +20,41 @@ post '/teams' do
     user = User.find(session[:id])
     new_team = Team.create(name: params[:team_name], user_id: user.id )
     TeamPlayer.create(player_id: params[:player], team_id: new_team.id)
+    redirect '/'
+  end
+end
+
+get '/teams/:id/edit' do |id|
+  @team = Team.find(id)
+  erb :team_edit
+end
+
+put '/teams/:id' do |id|
+  Team.find(id).update_attributes(name: params[:team_name])
+  redirect "/teams/#{id}"
+end
+
+get '/teams/:id/delete' do |id|
+  @team = Team.find(id)
+  erb :team_delete
+end
+
+delete '/teams/:id' do |id|
+  Team.find(id).destroy
+  redirect "/users/#{session[:id]}"
+end
+
+put '/teams/:id/vote' do |id|
+  team = Team.find(id)
+  if session[:id] == nil
+    # Anyone who is not logged in cannot vote
+    redirect '/login'
+  elsif team.user.id == session[:id]
+    # This prevents users from voting for their own teams
+    redirect '/'
+  else
+    team.score += 1
+    team.save
     redirect '/'
   end
 end
