cfcc276658cf0c11cd1b4e56794c671e105cb1b2
Kenn <kjb085@gmail.com>
2015-02-18 10:48:15 -0500

    Finish all preliminary auth routes

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,11 +1,40 @@
 get '/' do
-  erb :welcome
+  @teams = Team.all
+  erb :main
 end
 
 get '/login' do
-  
+  erb :login
 end
 
-get '/signup' do
-  
+post '/login' do
+  user = User.find_by(username: params[:username])
+  if user.try(:authenticate, params[:password])
+    session[:id] = user.id
+    redirect "/"
+    # {status: 200, path: "/"}
+  else
+    redirect '/login'
+    #401
+  end
+  #Change to above comments here when implementing AJAX
+end
+
+post '/signup' do
+
+  user = User.new(params[:new_user])
+
+  if user.save
+    session[:id] = user.id
+    redirect "/"
+  else
+    # Try throwing an error here as well
+    redirect '/login'
+  end
+end
+
+get '/logout' do
+  # Check this because redirecting in a get route
+  session.clear
+  redirect '/'
 end
