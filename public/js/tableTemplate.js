4a969fd5749b4383529dc050c01be013e5b5fa02
Kenn <kjb085@gmail.com>
2015-02-18 20:52:17 -0500

    Begin creating a template for use in an ajax call

---
+++ public/js/tableTemplate.js
@@ -0,0 +1,3 @@
+var tableHTML = (function(name, position, pro_team, age){
+  "<tr>" + "<td>" + name + "</td>" + "<td>" + position + "</td>" + "<td>" + pro_team + "</td>" + "<td>" + age "</td>" + "</tr>"
+})();
