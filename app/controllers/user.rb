aab42db8c3ed9cb9ea24b6a8e72957df918f7c7b
Kenn <kjb085@gmail.com>
2015-02-18 20:39:39 -0500

    Complete user controller to handle rendering profile pages and modifying the user resource

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -2,3 +2,27 @@ get '/users/:id' do |id|
   @user = User.find(id)
   erb :user_show
 end
+
+get '/users/:id/edit' do |id|
+  @user = User.find(id)
+  erb :user_edit
+end
+
+put '/users/:id' do |id|
+  params[:edit].delete_if do |key, value|
+    value == ""
+  end
+  User.find(id).update_attributes(params[:edit])
+  redirect "/users/#{id}"
+end
+
+get '/users/:id/delete' do |id|
+  @user = User.find(id)
+  erb :user_delete
+end
+
+delete '/users/:id' do |id|
+  User.find(id).destroy
+  session.clear
+  redirect '/'
+end
